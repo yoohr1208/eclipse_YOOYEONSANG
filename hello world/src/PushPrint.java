81203262f6ebaa4b23c795b29c666ad47071667f
yoohr1208 <73217226+yoohr1208@users.noreply.github.com>
2020-11-18 00:38:14 +0900

    Update PushPrint.java
    GitHub for git pull fighting!

--- hello world/src/PushPrint.java
+++ hello world/src/PushPrint.java
@@ -1,8 +1,9 @@
 
 public class PushPrint {
 	public static void main(String[] args) {
-		System.out.println("sophomore : 2�г�");
+		System.out.println("sophomore : 2ÇÐ³â");
 		System.out.println("Wankwang Univ.");
 		System.out.println("Jeonbuk Iksan");
+		System.out.println("fighting!");
 	}
 }
