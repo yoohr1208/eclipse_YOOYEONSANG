cf60a963be091de227c226eaa7841486be095bea
yoohr1208 <73217226+yoohr1208@users.noreply.github.com>
2020-11-18 00:06:29 +0900

    Update PushPrint.java
    PushPrint.java modified on GitHub

--- hello world/src/PushPrint.java
+++ hello world/src/PushPrint.java
@@ -1,6 +1,7 @@
 
 public class PushPrint {
 	public static void main(String[] args) {
-		System.out.println("sophomore : 2�г�");
+		System.out.println("sophomore : 2ÇÐ³â");
+		System.out.println("Jeonbuk Iksan");
 	}
 }
